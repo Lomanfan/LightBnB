6f35cfefba031e288192ca34e60ca25ecf5317f8
Liuming <lfan.canada@gmail.com>
2021-04-01 04:34:31 +0000

    ERD of LightBnB

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,   --FK--
 
   title VARCHAR(255) NOT NULL,
   description TEXT,
@@ -36,15 +36,15 @@ CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,   --FK--
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE            --FK--
 );
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,              --FK--
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,            --FK--
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,            --FK--
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );

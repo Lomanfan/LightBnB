0aab1c23fd20d57a023f9cc582d9f878092393d3
Liuming <lfan.canada@gmail.com>
2021-04-28 16:06:14 +0000

    INSERT queries to add data to all of the tables

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO users (name, email, password)
 VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
   ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),

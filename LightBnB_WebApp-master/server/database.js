327b652031725168309e879c8ec194f2111aa694
Liuming <lfan.canada@gmail.com>
2021-04-30 03:23:44 +0000

    use parameterized query on getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -102,21 +102,46 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
-  // console.log(limit);
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log("getAllPropertiesFromDB",err.message);
-    });
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE TRUE`;
+
+  if(options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` AND city LIKE $${queryParams.length} `;
+  }
+
+  if(options.owner_id) {
+    console.log("owner_id", options.owner_id);
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` AND owner_id = $${queryParams.length} `;
+  }
+
+  if(options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(parseInt(options.minimum_price_per_night) * 100);
+    queryParams.push(parseInt(options.maximum_price_per_night) * 100);
+    queryString += ` AND cost_per_night BETWEEN $${queryParams.length-1} AND $${queryParams.length} `;
+  }
+
+  queryString += ` GROUP BY properties.id, property_reviews.rating `;
+  if(options.minimum_rating) {
+    queryParams.push(parseInt(options.minimum_rating));
+    queryString += ` HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  queryString += ` ORDER BY cost_per_night `;
+
+  queryParams.push(limit);
+  queryString += ` LIMIT $${queryParams.length};`;
+
+  // console.log({queryString, queryParams});
+  return pool.query(queryString, queryParams).then((res) => {console.log("-----------------",res.rows); return res.rows});
 };
+
 exports.getAllProperties = getAllProperties;
 
 

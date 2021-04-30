6209c2ffe0a370c583f1927354f478534b602a5b
Liuming <lfan.canada@gmail.com>
2021-04-30 05:03:28 +0000

    remove one space

--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -1,7 +1,7 @@
 module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
-    database.getAllProperties(req.query, 20)
+    database.getAllProperties(req.query, 10)
     .then(properties => res.send({properties}))
     .catch(e => {
       console.error(e);
